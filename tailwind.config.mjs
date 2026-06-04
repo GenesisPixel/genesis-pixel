3fd519ed8ad5452e8b19888fb00a285452061c01
sebastianvasquezechavarria1234 <sebasvasquez1314@gmail.com>
2026-06-04 12:07:02 -0500

    feat: implement dynamic theme colors and global CSS variables

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: 'var(--color-bg)',
+        text: 'var(--color-text)',
+      },
+    },
   },
   plugins: [],
 };
